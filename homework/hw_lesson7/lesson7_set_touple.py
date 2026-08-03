f459f295ce2f6f3e2fb96d1b85cef62e22fc32c6
e.gerasimchuck <e.gerasimchuk@fstravel.com>
2026-08-03 17:10:38 +0400

    Работа с заданием 7

---
+++ homework/hw_lesson7/lesson7_set_touple.py
@@ -0,0 +1,18 @@
+fruits = {"яблоко", "банан"}
+fruits.add("апельсин")
+fruits.update(["груша", "виноград"])
+fruits.discard("банан")
+fruits.discard("киви")
+#fruits.remove("киви")
+fruit = fruits.pop()
+print(fruits)
+
+coordinates = (10, 20, 30, 20, 10, 20, 40)
+print(coordinates[0])
+print(coordinates[-1])
+print(coordinates[1:4])
+print(30 in coordinates)
+print(coordinates.index(20))
+print(coordinates.count(20))
+print(coordinates.count(50))
+print(len(coordinates))
